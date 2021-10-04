a91b7c287ae4d13b6dd39a7d1dfd970387861717
Marta <ubuntu@example.com>
2021-10-04 10:36:28 +0300

    homework_3_task_2_new

--- homework_3/homework_3_task2.py
+++ homework_3/homework_3_task2.py
@@ -2,21 +2,24 @@
 # Реализовать генератор, который будет выводить числа фибоначчи.
 
 
+# 1 способ, когда числа фибонначчи с 0 начинаются:
 def generator_fib(num):
     list_fib = [0, 1]
-    result_fib = [0,1]
-    if num ==0:
-        print(result_fib)
     while num > 0:
-        yield result_fib
+        yield list_fib[0]
         a = sum(list_fib)
-        result_fib.append(a)
         list_fib[0] = sum(list_fib)
-        list_fib.append(1)
         list_fib.reverse()
         num -= 1
+
+print(list(generator_fib(10)))
+
+# 2 способ, когда числа фибонначчи с 1 начинаются:
+def generator_fib_2(num):
+    fib_1, fib_2 = 0, 1
+    while num > 0:
+        fib_1, fib_2 = fib_2, fib_1 + fib_2
+        yield fib_1
+        num -= 1
         
-
-
-fibonnachi = generator_fib(4)
-print(fibonnachi)
+print(list(generator_fib_2(10)))
